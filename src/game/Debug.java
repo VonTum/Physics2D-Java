b414f1e10ea58ac90dcb6f8cd7612801cf8b3796
VonTum <lennart.vanhirtum@gmail.com>
2018-08-14 20:22:40 +0200

    Added extra debug info

--- src/game/Debug.java
+++ src/game/Debug.java
@@ -21,7 +21,10 @@ public class Debug {
 	private static String DEBUG_INFO = "";
 	
 	private static CircularQueue lastNanos = new CircularQueue(500);
-	private static CircularQueue lastTPS = new CircularQueue(20);
+	private static CircularQueue lastTPS = new CircularQueue(40);
+	
+	private static int age = 0;
+	private static long startTime = System.nanoTime();
 	
 	private static int curTPS = 0;
 	private static long lastTPSStore = System.nanoTime();
@@ -53,15 +56,17 @@ public class Debug {
 		INTERACTION_COUNT++;
 	}
 	public static synchronized void startTick(){
+		age++;
+		BEGIN_NANOS = System.nanoTime();
 	}
 	public static synchronized void endTick(){
 		long curTime = System.nanoTime();
 		long TICK_TIME_NANOS = curTime - BEGIN_NANOS;
-		BEGIN_NANOS = System.nanoTime();
+		
 		
 		curTPS++;
 		
-		if(curTime > lastTPSStore + 1000000000){
+		if(curTime > lastTPSStore + 100000000){ // 10^8 = 0.1sec
 			lastTPS.put(curTPS);
 			curTPS = 0;
 			lastTPSStore = curTime;
@@ -69,14 +74,16 @@ public class Debug {
 		
 		lastNanos.put(TICK_TIME_NANOS);
 		
-		double avgTick = lastNanos.sum()/500.0;
+		double avgTick = lastNanos.sum() / Math.min(500.0, age);
 		
-		double TPS = lastTPS.sum() / 20.0;
+		double TPS = lastTPS.sum() / Math.min(4.0, Math.floor((curTime-startTime)/1E9));
 		
-		DEBUG_INFO = 	"Tick: "+avgTick*0.000001 + "ms" + 
-						"\nTPS: " + TPS + 
+		DEBUG_INFO = 	"Tick: "+String.format("%.6fms", avgTick*0.000001) + 
+						"\nTPS: " + String.format("%.2f", TPS) + 
+						"\nTPSTarget: " + String.format("%.2f", Physics2D.SIMULATION_SPEED*Physics2D.SIMULATION_REPEATS_PER_TICK*100) + 
 						"\nInteractions: " + INTERACTION_COUNT + 
-						"\nObjects: " + world.physicals.size();
+						"\nObjects: " + world.physicals.size() + 
+						"\nAge: " + age + " ticks";
 		
 		INTERACTION_COUNT = 0;
 		
