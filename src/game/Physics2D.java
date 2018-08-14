059b53e6023698ade8796beeb8a61e7c848f928e
VonTum <lennart.vanhirtum@gmail.com>
2018-08-14 20:21:57 +0200

    Switched to ScheduledExecutor instead of Timer for tick

--- src/game/Physics2D.java
+++ src/game/Physics2D.java
@@ -6,8 +6,9 @@ import game.input.InputHandler;
 import game.input.StandardInputHandler;
 
 import java.io.IOException;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import math.CFrame;
 import math.Vec2;
@@ -29,30 +30,28 @@ public class Physics2D {
 		Debug.setWorld(w);
 		Screen.init(handler);
 		
-		new PyramidWorld(20).build(w);
+		new ConstrainedWorld().build(w);
 		
 		w.addObject(ObjectLibrary.createHammer(new CFrame(3.0, 0.0), new PhysicalProperties(1000, 0.05, 0.0, Color.DARK_GREY.alpha(0.6)), new PhysicalProperties(10.0, 0.05, 0.0, Color.DEFAULT_BRICK_COLOR)));
-		w.addObject(ObjectLibrary.createFloor(new CFrame(0.0, 0.0), new PhysicalProperties(10.0)));
+		// w.addObject(ObjectLibrary.createFloor(new CFrame(0.0, 0.0), new PhysicalProperties(10.0)));
 		
-		Timer worldRefresh = new Timer(true);
+		ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 		
-		// Screen.camera = w.objects.get(3).cframe;
-		
-		worldRefresh.schedule(new TimerTask() {
-			@Override public void run() {
-				if(!SIMULATION_PAUSED){
-					for(int i = 0; i < SIMULATION_REPEATS_PER_TICK * SIMULATION_SPEED; i++){
-						Debug.startTick();
-						w.tick(deltaT);
-						Debug.endTick();
-					}
+		executor.scheduleAtFixedRate(() -> {
+			if(!SIMULATION_PAUSED){
+				for(int i = 0; i < (int) SIMULATION_REPEATS_PER_TICK * SIMULATION_SPEED; i++){
+					Debug.startTick();
+					w.tick(deltaT);
+					Debug.endTick();
 				}
 			}
-		}, 0, (int) (10));
+		}, 0, 10, TimeUnit.MILLISECONDS);
 		
 		Screen.setWorld(w);
 		while(!Screen.shouldClose()){
 			Screen.refresh();
 		}
+		
+		executor.shutdownNow();
 	}
 }
