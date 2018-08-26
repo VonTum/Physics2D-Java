61cd4c1e7125bcb5a04d333601e4d3c9c5ecba22
VonTum <lennart.vanhirtum@gmail.com>
2018-08-27 00:03:46 +0200

    Added extra physics tests

--- tests/physics/tests/PhysicsTest.java
+++ tests/physics/tests/PhysicsTest.java
@@ -23,43 +23,11 @@ public class PhysicsTest {
 	
 	private PhysicalProperties properties = new PhysicalProperties(10.0, 0.05, 0.0, Color.DEFAULT_BRICK_COLOR);
 	
-	private World basicWorld, noGravityWorld;
-	private Physical noGravityBox;
-	
-	@Before
-	public void setup() {
-		basicWorld = new World(new Vec2(0.0, -1.0));
-		basicWorld.addObject(ObjectLibrary.createFloor(new CFrame(0.0, 0.0), properties));
-		basicWorld.addObject(new Box(new CFrame(0.0, 1.0, 0.3), 0.3, 0.1, properties));
-		basicWorld.addObject(new Box(new CFrame(0.0, 1.5, 0.3), 0.1, 0.1, properties));
-		basicWorld.addObject(new Box(new CFrame(0.0, 0.5, -0.1), 0.1, 0.3, properties));
-		
-		noGravityWorld = new World(Vec2.ZERO);
-		noGravityBox = new Box(new CFrame(0.0, 0.0), 0.3, 0.1, properties);
-		noGravityWorld.addObject(noGravityBox);
-	}
-	
-	public void assertNotFlipping(World w) {
-		for(Physical p:w.physicals)
-			assertNotFlipping(p);
-	}
-	
-	public void assertNotFlipping(Physical p) {
-		assertTrue("Object " + p + " moving too fast! " + p.velocity, p.velocity.isShorterThan(MAX_VELOCITY));
-		assertTrue("Object " + p + " rotating too fast! " + p.angularVelocity, p.angularVelocity < MAX_ANGULAR_VELOCITY);
-	}
-	
-	@Test
-	public void testNotFlipping() {
-		for(int i = 0; i < 1000; i++){
-			basicWorld.tick(0.01);
-			assertNotFlipping(basicWorld);
-		}
-	}
+	private Box testBox = new Box(CFrame.IDENTITY, 0.3, 0.1, properties);
 	
 	@Test
 	public void testAcceleration(){
-		Box testBox = new Box(new CFrame(0.0, 0.0), 0.3, 0.1, properties);
+		// Box testBox = new Box(new CFrame(0.0, 0.0), 0.3, 0.1, properties);
 		
 		Vec2 localAttachPoint = new Vec2(0.15, 0.0);
 		Vec2 desiredAcceleration = Vec2.UNITY.mul(0.5);
@@ -82,7 +50,7 @@ public class PhysicsTest {
 	
 	@Test
 	public void testPreservationOfEnergy(){
-		Box testBox = new Box(new CFrame(0.0, 0.0), 0.3, 0.1, properties);
+		// Box testBox = new Box(new CFrame(0.0, 0.0), 0.3, 0.1, properties);
 		testBox.velocity = new Vec2(0.0, -5.0);
 		
 		Vec2 gravity = Vec2.UNITNEGY;
@@ -98,18 +66,62 @@ public class PhysicsTest {
 	}
 	
 	@Test
+	public void testInertiaThroughCenter(){
+		// Box testBox = new Box(new CFrame(0.0, 0.0), 0.6, 0.6, properties);
+		
+		assertEquals(testBox.getMass(), testBox.getPointInertia(Vec2.ZERO, Vec2.UNITX), DELTA);
+		assertEquals(testBox.getMass(), testBox.getPointInertia(Vec2.ZERO, Vec2.UNITY), DELTA);
+		assertEquals(testBox.getMass(), testBox.getPointInertia(Vec2.ZERO, Vec2.UNITNEGX), DELTA);
+		assertEquals(testBox.getMass(), testBox.getPointInertia(Vec2.ZERO, Vec2.UNITNEGY), DELTA);
+		
+		Vec2 offset = new Vec2(0.3, 0.7);
+		
+		assertEquals(testBox.getMass(), testBox.getPointInertia(offset, offset.neg().normalize()), DELTA);
+	}
+	
+	@Test
 	public void testImpulse(){
-		Box box = new Box(new CFrame(0.0, 0.0), 0.6, 0.6, properties);
+		// Box testBox = new Box(new CFrame(0.0, 0.0), 0.6, 0.6, properties);
 		
-		Vec2 point = new Vec2(0.3, -0.3);
+		Vec2 point = new Vec2(0.15, -0.05);
 		
-		Vec2 accel = new Vec2(0.7, 0.3);
+		Vec2 accel = new Vec2(0.0, 0.7);
 		
-		double inertia = box.getPointInertia(point, accel.normalize());
+		
+		
+		double inertia = testBox.getPointInertia(point, accel.normalize());
+		
+		
 		
 		Vec2 impulse = accel.mul(inertia);
-		box.applyImpulse(impulse, point);
+		testBox.applyImpulse(impulse, point);
 		
-		assertEquals(accel.length(), box.getSpeedOfPoint(point).dot(accel.normalize()), DELTA);
+		System.out.println("Accel: " + accel + "\nInertia: " + inertia + "\nImpulse: " + impulse + "\nSpeed: " + testBox.getSpeedOfPoint(point));
+		System.out.println(testBox);
+		
+		assertEquals(accel.y, testBox.getSpeedOfPoint(point).y, DELTA);
+	}
+	
+	@Test
+	public void testGetAccellerationOfPoint(){
+		Vec2 relativePos = new Vec2(0.15, 0.05);
+		
+		double deltaT = 0.00001;
+		
+		testBox.velocity = new Vec2(2.0, 0.7);
+		testBox.angularVelocity = 3.3;
+		
+		Vec2 lastVelocity = testBox.getSpeedOfPoint(relativePos);
+		
+		for(int i = 0; i < 100000; i++){
+			Vec2 globalPos = testBox.cframe.localToGlobal(relativePos);
+			testBox.applyForce(new Vec2(-0.3, 0.7), globalPos);
+			Vec2 acceleration = testBox.getAccelerationOfPoint(globalPos);
+			Vec2 curVelocity = testBox.getSpeedOfPoint(globalPos);
+			// very high tolerance for error on this one, discretization errors are to be expected
+			assertVecEquals(curVelocity.subtract(lastVelocity), acceleration.mul(deltaT), 1E-4); 
+			lastVelocity = curVelocity;
+			testBox.update(deltaT);
+		}
 	}
 }
